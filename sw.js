09dc79ffd75b050c1e9436d9ddc8d4feadafa055
ThomasSimon04 <thomassimon2004@gmx.de>
2026-08-11 16:04:36 +0200

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,30 @@
+/* Blick – Service Worker.
+   WICHTIG: Nach jeder Änderung an index.html die Versionsnummer erhöhen,
+   sonst zeigt das Handy weiter die alte Fassung. */
+const VERSION = 'blick-v1';
+const FILES = ['./', './index.html', './manifest.webmanifest', './icon-192.png', './icon-512.png'];
+
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open(VERSION).then(c => c.addAll(FILES)).then(() => self.skipWaiting()));
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys()
+      .then(keys => Promise.all(keys.filter(k => k !== VERSION).map(k => caches.delete(k))))
+      .then(() => self.clients.claim())
+  );
+});
+
+self.addEventListener('fetch', e => {
+  if (e.request.method !== 'GET') return;
+  e.respondWith(
+    fetch(e.request)
+      .then(res => {
+        const copy = res.clone();
+        caches.open(VERSION).then(c => c.put(e.request, copy));
+        return res;
+      })
+      .catch(() => caches.match(e.request).then(r => r || caches.match('./index.html')))
+  );
+});
